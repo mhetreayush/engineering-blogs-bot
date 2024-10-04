d6994dec732163c5384397269d172555288fe1ba
mhetreayush <mhetreayush1719@gmail.com>
2024-10-04 20:33:31 +0530

    fix keep alive

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,14 @@
 import { ENV } from '@src/constants/env';
 import { blogRouter } from '@src/routes/blog-router';
+import axios from 'axios';
 import bodyParser from 'body-parser';
 import { Express } from 'express';
 import express from 'express';
 import cron from 'node-cron'; // Import node-cron here
 
-import { keepServerAlive } from './utils/keep-server-alive';
 import { sendDailyBlog } from './utils/send-daily-blog.controller';
+
+// const MINUTE = 1000 * 60;
 
 const app: Express = express();
 
@@ -25,14 +27,19 @@ app.get('/keep-me-alive', (req, res) => {
   res.send(message);
 });
 
-keepServerAlive();
-
 // Schedule the job to run at 7:30 AM IST every day
 cron.schedule('30 1 * * *', () => {
   // 1 AM UTC is 7:30 AM IST
   console.log('Running scheduled task to send WhatsApp message...');
   sendDailyBlog();
 });
+
+if (ENV.NODE_ENV === 'production') {
+}
+
+setInterval(() => {
+  axios.get(`${ENV.BASE_URL}${ENV.NODE_ENV === 'production' ? '' : `:${ENV.PORT}`}/keep-me-alive`);
+}, 1000); // Logs message every 10 minutes
 
 // Start the Express server
 app.listen(PORT, () => {
