dd1a1026babcb57a4d66df5b42776ef4aeb743b6
mhetreayush <mhetreayush1719@gmail.com>
2024-10-04 21:38:00 +0530

    update cron timings

--- src/utils/create-cron-schedules.ts
+++ src/utils/create-cron-schedules.ts
@@ -9,22 +9,24 @@ const scheduleBlogCron = () => {
 
 const CRON_TIMES = [
   {
-    description: 'Every day at 7:30 AM IST',
-    cronValue: '30 1 * * *',
+    description: 'Every day at 7:30 AM',
+    cronValue: '30 7 * * *', // 7:30 AM IST
   },
   {
     description: 'Every day at 4 PM IST',
-    cronValue: '00 10 * * *',
+    cronValue: '00 16 * * *', // 4:00 PM IST
   },
   {
     description: 'Every day at 9:30 PM IST',
-    cronValue: '30 16 * * *',
+    cronValue: '30 21 * * *', // 9:30 PM IST
   },
 ];
 
 export const createCronSchedules = () => {
   CRON_TIMES.forEach(({ cronValue, description }) => {
     console.log(`Scheduling task to run at: ${description}`);
-    cron.schedule(cronValue, scheduleBlogCron);
+    cron.schedule(cronValue, scheduleBlogCron, {
+      timezone: 'Asia/Kolkata',
+    });
   });
 };
