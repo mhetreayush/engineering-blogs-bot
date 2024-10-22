8781717acc044ab357a8b44b9782b44e98c7a3a3
mhetreayush <mhetreayush1719@gmail.com>
2024-10-22 22:10:24 +0530

    send interaction reminder to only those who have active links

--- src/utils/send-daily-reminder-to-interact.ts
+++ src/utils/send-daily-reminder-to-interact.ts
@@ -1,6 +1,42 @@
-import UserModel from '@src/models/User.model';
+import LinkModel from '@src/models/Link.model';
 
 import { sendWhatsAppMessage } from './send-whatsapp-message';
+
+const getUsersWithUnsentLinks = async () => {
+  try {
+    const result = await LinkModel.aggregate([
+      {
+        $match: {
+          is_sent: false,
+        },
+      },
+      {
+        $group: {
+          _id: '$user_phone',
+          unsentLinksCount: { $sum: 1 },
+        },
+      },
+      {
+        $match: {
+          unsentLinksCount: { $gt: 0 },
+        },
+      },
+      {
+        $project: {
+          _id: 0,
+          user_phone: '$_id',
+        },
+      },
+    ]);
+
+    return result;
+  } catch (error) {
+    console.error('Error fetching users with unsent links:', error);
+    throw error;
+  }
+};
+
+export default getUsersWithUnsentLinks;
 
 export const sendDailyRemainderToInteract = async () => {
   try {
@@ -8,10 +44,11 @@ export const sendDailyRemainderToInteract = async () => {
         Hey! 👋 \nJust a quick note: \nFor the bot to keep working as expected, We need you to interact with it once in every 24 hours as WhatsApp maintains 24-hour sessions, so this helps keep things running smoothly. \nYou can simply do it by reacting to this message!\nThanks for helping out! 😊
       `;
 
-    const users = await UserModel.find().select('phone_number');
+    const users = await getUsersWithUnsentLinks();
+
     const userMessagesPromise = users.map(async (user) => {
       await sendWhatsAppMessage({
-        recipient_number: user.phone_number,
+        recipient_number: user.user_phone,
         message,
       });
     });
